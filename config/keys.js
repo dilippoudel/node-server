f06324066775611bb090657cfad78a82e7d8dbc6
Dilip Poudel <dilippoudel@Dilip-MacBook-Pro.local>
2019-11-23 08:51:39 +0200

    updated files

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+ googleClientID:'954379961198-tfudgeucf8rgbq3oeu25mqbn0fcvm7c4.apps.googleusercontent.com',
+ googleClientSecrete: '5BrCJ6PJYmEDqO15rS28Ja70',
+ mongoURI: 'mongodb+srv://dilip123:aWbFt78PGVSIqI7r@emaily-oiokm.mongodb.net/test?retryWrites=true&w=majority'
+
+}
